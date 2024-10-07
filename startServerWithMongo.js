330af86357682a099a6d6c4f1cf1ca46c3c914a2
JayaniVimukthika <144791020+JayaniVimukthika@users.noreply.github.com>
2024-10-07 21:00:38 +1100

    6.2 commit

--- startServerWithMongo.js
+++ startServerWithMongo.js
@@ -2,7 +2,7 @@ let express = require('express');
 let app = express();
 // const { MongoClient, ServerApiVersion } = require('mongodb');
 // // const uri
-// const uri = "mongodb+srv://s220194805:nC0IFkpgBCS1fp0q@cluster0.k3wz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+const uri = "mongodb+srv://s220194805:nC0IFkpgBCS1fp0q@cluster0.k3wz2.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 let port = process.env.port || 3040;
 require('./dbConnection');
 
@@ -33,34 +33,7 @@ async function runDBConnection() {
     }
 }
 
-// app.get('/', function (req,res) {
-//     res.render('indexMongo.html');
-// });
 
-// app.get('/api/cats', (req,res) => {
-//     getAllCats((err,result)=>{
-//         if (!err) {
-//             res.json({statusCode:200, data:result, message:'get all data successful'});
-//         }
-//     });
-// });
-
-// app.post('/api/cat', (req,res)=>{
-//     let cat = req.body;
-//     postCat(cat, (err, result) => {
-//         if (!err) {
-//             res.json({statusCode:201, data:result, message:'success'});
-//         }
-//     });
-// });
-
-// function postCat(cat,callback) {
-//     collection.insertOne(cat,callback);
-// }
-
-// function getAllCats(callback){
-//     collection.find({}).toArray(callback);
-// }
 
 app.listen(port, ()=>{
     console.log('express server started');
